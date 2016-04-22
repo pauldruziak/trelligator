c304ae44f7c878d70c7a3c8c7706eb691e134ddf
Pavel Druziak <pavel.druzyak@gmail.com>
2016-04-22 18:25:34 +0300

    Fix test

--- test/app_test.rb
+++ test/app_test.rb
@@ -27,7 +27,7 @@ class WebhookWithStatusChangedTest < Minitest::Test
   def test_webhook
     mock = MiniTest::Mock.new
     mock.expect(:update_status, true) do |args|
-      args[:state] == 'pending' && args[:description] == 'Moved from QA to Review by James Bond'
+      args[:state] == 'pending' && args[:description] == "James Bond moved to 'Review' from 'QA'"
     end
     VCR.use_cassette('pull_requests', record: :none) do
       Trelligator::GithubPullRequest.stub(:new, mock) do
